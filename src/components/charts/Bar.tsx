66fff7db22744a70afb01606fa2d4d7b59c37388
Egi Gagah Brilliant <egigagahb19@gmail.com>
2022-10-09 11:26:52 +0700

    add skeleton for chart

--- src/components/charts/Bar.tsx
+++ src/components/charts/Bar.tsx
@@ -1,117 +1,149 @@
-import { BoxProps } from "@chakra-ui/react";
+import {
+    BoxProps,
+    Flex,
+    HStack,
+    Skeleton,
+    SkeletonProps,
+} from "@chakra-ui/react";
 import { ResponsiveBar } from "@nivo/bar";
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 
 interface BarProps extends BoxProps {
     data: any;
 }
 
-const App = ({ data }: BarProps): JSX.Element => (
-    <ResponsiveBar
-        data={data}
-        keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-        indexBy="country"
-        margin={{ top: 50, right: 25, bottom: 50, left: 60 }}
-        padding={0.3}
-        groupMode="grouped"
-        valueScale={{ type: "linear" }}
-        indexScale={{ type: "band", round: true }}
-        colors={{ scheme: "nivo" }}
-        defs={[
-            {
-                id: "dots",
-                type: "patternDots",
-                background: "inherit",
-                color: "#38bcb2",
-                size: 4,
-                padding: 1,
-                stagger: true,
-            },
-            {
-                id: "lines",
-                type: "patternLines",
-                background: "inherit",
-                color: "#eed312",
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10,
-            },
-        ]}
-        fill={[
-            {
-                match: {
-                    id: "fries",
-                },
-                id: "dots",
-            },
-            {
-                match: {
-                    id: "sandwich",
-                },
-                id: "lines",
-            },
-        ]}
-        borderColor={{
-            from: "color",
-            modifiers: [["darker", 1.6]],
-        }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "country",
-            legendPosition: "middle",
-            legendOffset: 32,
-        }}
-        axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "food",
-            legendPosition: "middle",
-            legendOffset: -40,
-        }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor={{
-            from: "color",
-            modifiers: [["darker", 1.6]],
-        }}
-        // legends={[
-        //     {
-        //         dataFrom: "keys",
-        //         anchor: "bottom-right",
-        //         direction: "column",
-        //         justify: false,
-        //         translateX: 120,
-        //         translateY: 0,
-        //         itemsSpacing: 2,
-        //         itemWidth: 100,
-        //         itemHeight: 20,
-        //         itemDirection: "left-to-right",
-        //         itemOpacity: 0.85,
-        //         symbolSize: 20,
-        //         effects: [
-        //             {
-        //                 on: "hover",
-        //                 style: {
-        //                     itemOpacity: 1,
-        //                 },
-        //             },
-        //         ],
-        //     },
-        // ]}
-        role="application"
-        ariaLabel="Nivo bar chart demo"
-        barAriaLabel={function (e) {
-            return (
-                e.id + ": " + e.formattedValue + " in country: " + e.indexValue
-            );
-        }}
-    />
-);
+const App = ({ data }: BarProps): JSX.Element => {
+    const [isLoaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setLoaded(true);
+    }, []);
+
+    return (
+        <>
+            {!isLoaded && (
+                <BarSkeleton isLoaded={isLoaded} width="2rem" height="80%" />
+            )}
+            {isLoaded && (
+                <ResponsiveBar
+                    data={data}
+                    keys={[
+                        "hot dog",
+                        "burger",
+                        "sandwich",
+                        "kebab",
+                        "fries",
+                        "donut",
+                    ]}
+                    indexBy="country"
+                    margin={{ top: 50, right: 25, bottom: 50, left: 60 }}
+                    padding={0.3}
+                    groupMode="grouped"
+                    valueScale={{ type: "linear" }}
+                    indexScale={{ type: "band", round: true }}
+                    colors={{ scheme: "nivo" }}
+                    defs={[
+                        {
+                            id: "dots",
+                            type: "patternDots",
+                            background: "inherit",
+                            color: "#38bcb2",
+                            size: 4,
+                            padding: 1,
+                            stagger: true,
+                        },
+                        {
+                            id: "lines",
+                            type: "patternLines",
+                            background: "inherit",
+                            color: "#eed312",
+                            rotation: -45,
+                            lineWidth: 6,
+                            spacing: 10,
+                        },
+                    ]}
+                    fill={[
+                        {
+                            match: {
+                                id: "fries",
+                            },
+                            id: "dots",
+                        },
+                        {
+                            match: {
+                                id: "sandwich",
+                            },
+                            id: "lines",
+                        },
+                    ]}
+                    borderColor={{
+                        from: "color",
+                        modifiers: [["darker", 1.6]],
+                    }}
+                    axisTop={null}
+                    axisRight={null}
+                    axisBottom={{
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: 0,
+                        legend: "country",
+                        legendPosition: "middle",
+                        legendOffset: 32,
+                    }}
+                    axisLeft={{
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: 0,
+                        legend: "food",
+                        legendPosition: "middle",
+                        legendOffset: -40,
+                    }}
+                    labelSkipWidth={12}
+                    labelSkipHeight={12}
+                    labelTextColor={{
+                        from: "color",
+                        modifiers: [["darker", 1.6]],
+                    }}
+                    // legends={[
+                    //     {
+                    //         dataFrom: "keys",
+                    //         anchor: "bottom-right",
+                    //         direction: "column",
+                    //         justify: false,
+                    //         translateX: 120,
+                    //         translateY: 0,
+                    //         itemsSpacing: 2,
+                    //         itemWidth: 100,
+                    //         itemHeight: 20,
+                    //         itemDirection: "left-to-right",
+                    //         itemOpacity: 0.85,
+                    //         symbolSize: 20,
+                    //         effects: [
+                    //             {
+                    //                 on: "hover",
+                    //                 style: {
+                    //                     itemOpacity: 1,
+                    //                 },
+                    //             },
+                    //         ],
+                    //     },
+                    // ]}
+                    role="application"
+                    ariaLabel="Nivo bar chart demo"
+                    barAriaLabel={function (e) {
+                        return (
+                            e.id +
+                            ": " +
+                            e.formattedValue +
+                            " in country: " +
+                            e.indexValue
+                        );
+                    }}
+                />
+            )}
+        </>
+    );
+};
 
 const Bar = memo(
     App,
@@ -119,3 +151,26 @@ const Bar = memo(
 );
 
 export default Bar;
+
+function BarSkeleton({ ...res }: SkeletonProps): JSX.Element {
+    return (
+        <HStack
+            spacing={4}
+            p={[4, 8]}
+            justifyContent="center"
+            alignItems="center"
+            as={Flex}
+            flex={1}
+            w="100%"
+            h="100%"
+            display={res.isLoaded ? "none" : "flex"}
+        >
+            <Skeleton {...res} />
+            <Skeleton {...res} />
+            <Skeleton {...res} />
+            <Skeleton {...res} />
+            <Skeleton {...res} />
+            <Skeleton {...res} />
+        </HStack>
+    );
+}
--- src/components/charts/Pie.tsx
+++ src/components/charts/Pie.tsx
@@ -1,169 +1,191 @@
 import {
-    Box,
-    BoxProps,
+    Flex,
+    Skeleton,
+    SkeletonCircle,
+    Stack,
+    StackProps,
     useBreakpoint,
-    useBreakpointValue,
 } from "@chakra-ui/react";
 import { ResponsivePie } from "@nivo/pie";
-import { memo, useEffect, useRef } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import { useSize } from "@chakra-ui/react-use-size";
 
-interface PieProps extends BoxProps {
+interface PieProps extends StackProps {
     data: any;
 }
 
 const App = ({ data, ...res }: PieProps): JSX.Element => {
+    const [isLoaded, setLoaded] = useState(false);
     const ref = useRef<HTMLDivElement | null>(null);
     const dimension = useSize(ref);
     const breakpoint = useBreakpoint();
 
+    useEffect(() => {
+        if (ref && dimension && breakpoint) setLoaded(true);
+    }, [ref, dimension, breakpoint]);
+
     return (
-        <Box
+        <Stack
             {...res}
             ref={ref}
             w={res?.w || res?.width || "100%"}
             h={res?.h || res?.height || "auto"}
-            // position={res?.position || "relative"}
+            as={Flex}
+            justifyContent="center"
+            alignItems="center"
+            spacing={4}
         >
-            {dimension && (
-                <ResponsivePie
-                    data={data}
-                    margin={{
-                        top:
-                            (dimension?.height *
-                                (breakpoint !== "sm" ? 8 : 5)) /
-                            100,
-                        right:
-                            breakpoint !== "sm"
-                                ? (dimension?.height * 10) / 100
-                                : 0,
-                        bottom:
-                            (dimension?.height *
-                                (breakpoint !== "sm" ? 15 : 8)) /
-                            100,
-                        left:
-                            breakpoint !== "sm"
-                                ? (dimension?.height * 10) / 100
-                                : 0,
-                    }}
-                    innerRadius={0.5}
-                    padAngle={0.7}
-                    cornerRadius={3}
-                    activeOuterRadiusOffset={8}
-                    borderWidth={1}
-                    borderColor={{
-                        from: "color",
-                        modifiers: [["darker", 0.2]],
-                    }}
-                    enableArcLinkLabels={false}
-                    // arcLinkLabelsSkipAngle={10}
-                    // arcLinkLabelsTextColor="#333333"
-                    // arcLinkLabelsThickness={2}
-                    // arcLinkLabelsColor={{ from: "color" }}
-                    arcLabelsSkipAngle={10}
-                    arcLabelsTextColor={{
-                        from: "color",
-                        modifiers: [["darker", 2]],
-                    }}
-                    // defs={[
-                    //     {
-                    //         id: "dots",
-                    //         type: "patternDots",
-                    //         background: "inherit",
-                    //         color: "rgba(255, 255, 255, 0.3)",
-                    //         size: 4,
-                    //         padding: 1,
-                    //         stagger: true,
-                    //     },
-                    //     {
-                    //         id: "lines",
-                    //         type: "patternLines",
-                    //         background: "inherit",
-                    //         color: "rgba(255, 255, 255, 0.3)",
-                    //         rotation: -45,
-                    //         lineWidth: 6,
-                    //         spacing: 10,
-                    //     },
-                    // ]}
-                    // fill={[
-                    //     {
-                    //         match: {
-                    //             id: "ruby",
-                    //         },
-                    //         id: "dots",
-                    //     },
-                    //     {
-                    //         match: {
-                    //             id: "c",
-                    //         },
-                    //         id: "dots",
-                    //     },
-                    //     {
-                    //         match: {
-                    //             id: "go",
-                    //         },
-                    //         id: "dots",
-                    //     },
-                    //     {
-                    //         match: {
-                    //             id: "python",
-                    //         },
-                    //         id: "dots",
-                    //     },
-                    //     {
-                    //         match: {
-                    //             id: "scala",
-                    //         },
-                    //         id: "lines",
-                    //     },
-                    //     {
-                    //         match: {
-                    //             id: "lisp",
-                    //         },
-                    //         id: "lines",
-                    //     },
-                    //     {
-                    //         match: {
-                    //             id: "elixir",
-                    //         },
-                    //         id: "lines",
-                    //     },
-                    //     {
-                    //         match: {
-                    //             id: "javascript",
-                    //         },
-                    //         id: "lines",
-                    //     },
-                    // ]}
-                    legends={[
-                        {
-                            anchor: "bottom",
-                            direction: "row",
-                            justify: false,
-                            translateX: 0,
-                            translateY: (dimension.height * 10) / 100,
-                            itemsSpacing: 10,
-                            itemWidth:
-                                (dimension.width * 80) / 100 / data.length,
-                            itemHeight: 18,
-                            itemTextColor: "#999",
-                            itemDirection: "left-to-right",
-                            itemOpacity: 1,
-                            symbolSize: 18,
-                            symbolShape: "circle",
-                            effects: [
-                                {
-                                    on: "hover",
-                                    style: {
-                                        itemTextColor: "#000",
+            <SkeletonCircle
+                height={isLoaded ? "100%" : "70%"}
+                width={isLoaded ? "100%" : "70%"}
+                isLoaded={isLoaded}
+            >
+                {dimension && (
+                    <ResponsivePie
+                        data={data}
+                        margin={{
+                            top:
+                                (dimension?.height *
+                                    (breakpoint !== "sm" ? 8 : 5)) /
+                                100,
+                            right:
+                                breakpoint !== "sm"
+                                    ? (dimension?.height * 10) / 100
+                                    : 0,
+                            bottom:
+                                (dimension?.height *
+                                    (breakpoint !== "sm" ? 15 : 8)) /
+                                100,
+                            left:
+                                breakpoint !== "sm"
+                                    ? (dimension?.height * 10) / 100
+                                    : 0,
+                        }}
+                        innerRadius={0.5}
+                        padAngle={0.7}
+                        cornerRadius={3}
+                        activeOuterRadiusOffset={8}
+                        borderWidth={1}
+                        borderColor={{
+                            from: "color",
+                            modifiers: [["darker", 0.2]],
+                        }}
+                        enableArcLinkLabels={false}
+                        // arcLinkLabelsSkipAngle={10}
+                        // arcLinkLabelsTextColor="#333333"
+                        // arcLinkLabelsThickness={2}
+                        // arcLinkLabelsColor={{ from: "color" }}
+                        arcLabelsSkipAngle={10}
+                        arcLabelsTextColor={{
+                            from: "color",
+                            modifiers: [["darker", 2]],
+                        }}
+                        // defs={[
+                        //     {
+                        //         id: "dots",
+                        //         type: "patternDots",
+                        //         background: "inherit",
+                        //         color: "rgba(255, 255, 255, 0.3)",
+                        //         size: 4,
+                        //         padding: 1,
+                        //         stagger: true,
+                        //     },
+                        //     {
+                        //         id: "lines",
+                        //         type: "patternLines",
+                        //         background: "inherit",
+                        //         color: "rgba(255, 255, 255, 0.3)",
+                        //         rotation: -45,
+                        //         lineWidth: 6,
+                        //         spacing: 10,
+                        //     },
+                        // ]}
+                        // fill={[
+                        //     {
+                        //         match: {
+                        //             id: "ruby",
+                        //         },
+                        //         id: "dots",
+                        //     },
+                        //     {
+                        //         match: {
+                        //             id: "c",
+                        //         },
+                        //         id: "dots",
+                        //     },
+                        //     {
+                        //         match: {
+                        //             id: "go",
+                        //         },
+                        //         id: "dots",
+                        //     },
+                        //     {
+                        //         match: {
+                        //             id: "python",
+                        //         },
+                        //         id: "dots",
+                        //     },
+                        //     {
+                        //         match: {
+                        //             id: "scala",
+                        //         },
+                        //         id: "lines",
+                        //     },
+                        //     {
+                        //         match: {
+                        //             id: "lisp",
+                        //         },
+                        //         id: "lines",
+                        //     },
+                        //     {
+                        //         match: {
+                        //             id: "elixir",
+                        //         },
+                        //         id: "lines",
+                        //     },
+                        //     {
+                        //         match: {
+                        //             id: "javascript",
+                        //         },
+                        //         id: "lines",
+                        //     },
+                        // ]}
+                        legends={[
+                            {
+                                anchor: "bottom",
+                                direction: "row",
+                                justify: false,
+                                translateX: 0,
+                                translateY: (dimension.height * 10) / 100,
+                                itemsSpacing: 10,
+                                itemWidth:
+                                    (dimension.width * 80) / 100 / data.length,
+                                itemHeight: 18,
+                                itemTextColor: "#999",
+                                itemDirection: "left-to-right",
+                                itemOpacity: 1,
+                                symbolSize: 18,
+                                symbolShape: "circle",
+                                effects: [
+                                    {
+                                        on: "hover",
+                                        style: {
+                                            itemTextColor: "#000",
+                                        },
                                     },
-                                },
-                            ],
-                        },
-                    ]}
-                />
-            )}
-        </Box>
+                                ],
+                            },
+                        ]}
+                    />
+                )}
+            </SkeletonCircle>
+            <Skeleton
+                display={isLoaded ? "none" : "block"}
+                width="70%"
+                height="2.5rem"
+                isLoaded={isLoaded}
+            />
+        </Stack>
     );
 };
 
