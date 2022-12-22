7b5f64692900157cd8b486f0b7946fb38cb19925
Egi Gagah Brilliant <egigagahb19@gmail.com>
2022-12-22 12:30:45 +0700

    add env api url & rewrites

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 const withPlugins = require("next-compose-plugins");
 const { i18n } = require("./i18n");
 
-module.exports = withPlugins([], {
-    i18n,
-    experimental: {
-        esmExternals: false,
+module.exports = withPlugins(
+    [
+        {
+            async rewrites() {
+                return [
+                    {
+                        source: "/graphql",
+                        destination:
+                            (process.env.NEXT_PUBLIC_API_URL || "localhost") +
+                            "/graphql",
+                    },
+                ];
+            },
+        },
+    ],
+    {
+        i18n,
+        experimental: {
+            esmExternals: false,
+        },
     },
-});
+);
